57babfa5b39e1eda64e064a3bb0faefc90f3e68b
Maddox <na.testmail.na@gmail.com>
2018-09-11 11:06:23 +0200

    Update action-sayMAC-Network-Info.py

--- action-sayMAC-Network-Info.py
+++ action-sayMAC-Network-Info.py
@@ -38,17 +38,17 @@ def action_wrapper(hermes, intentMessage, conf):
 	try:
 		ip = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
 		mac = ni.ifaddresses('wlan0')[ni.AF_LINK][0]['addr']
-		iface = "wlan0"
+		iface = "W-Lan0"
 	except ValueError:
 		try:
 			ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 			mac = ni.ifaddresses('eth0')[ni.AF_LINK][0]['addr']
-			iface = "eth0"
+			iface = "ETH0"
 		except ValueError:
 			try:
 				ip = ni.ifaddresses('wlan1')[ni.AF_INET][0]['addr']
 				mac = ni.ifaddresses('wlan1')[ni.AF_LINK][0]['addr']
-				iface = "wlan1"
+				iface = "W-Lan1"
 			except ValueError:
 				err_code = 1
 	if err_code == 0:
